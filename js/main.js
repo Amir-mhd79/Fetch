866318c9c9dcc142ac93fa8a57bb80cfcf92453c
Amir-mhd79 <Amirmohamad.akbarloo@gmail.com>
2023-08-26 17:51:04 +0330

    Update the catch section

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 let button = document.querySelector("button.get");
 let fetchComments = async () => {
-  let html = "";
-  let data = await fetch("https://jsonplaceholder.typicode.com/comments").catch((err) =>
-      {document.querySelector("div.wrapper").innerText = err}
-  );
-  let res = await data.json();
-  res.forEach((elem) => {
-    html += `<div class="comment">
+  try {
+    let html = "";
+    let data = await fetch("https://jsonplaceholder.typicode.com/comments");
+    let res = await data.json();
+    res.forEach((elem) => {
+      html += `<div class="comment">
                 <h2 class="postId">postId: ${elem.postId}</h2>
                 <h2 class="id">ID: ${elem.id}</h2>
                 <h2 class="name">Name: ${elem.name}</h2>
                 <p class="email">Email: ${elem.email}</p>
                 <p class="body">Comment: ${elem.body}</p>
             </div>`;
-  });
-  document.querySelector("div.wrapper").innerHTML = html;
+    });
+    document.querySelector("div.wrapper").innerHTML = html;
+  } catch (error) {
+    document.querySelector("div.wrapper").innerHTML = error;
+  }
 };
 button.addEventListener("click", fetchComments);
